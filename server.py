4c2a334b35b5d42bec3e58b4612836e5be2bfba0
Greg Schafer <grschafer@gmail.com>
2015-09-22 05:18:36 +0000

    Allow multiple sequential connections

--- server.py
+++ server.py
@@ -3,11 +3,12 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('0.0.0.0', 8000))
 sock.listen(1)
-conn, addr = sock.accept()
-print('connected by', addr)
 while True:
-    data = conn.recv(1024)
-    if not data: break
-    conn.sendall(data)
-conn.close()
+    conn, addr = sock.accept()
+    print('connected by', addr)
+    while True:
+        data = conn.recv(1024)
+        if not data: break
+        conn.sendall(data)
+    conn.close()
 sock.close()

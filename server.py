9ba5cd9eca88f0ff17225d31a34cf6439bd545da
Greg Schafer <grschafer@gmail.com>
2015-09-23 05:01:22 +0000

    Sample middleware (caching)

--- server.py
+++ server.py
@@ -13,6 +13,18 @@ def echo_headers(request):
 def make_404(request):
     return '''HTTP/1.1 404 Not Found\r\n\r\nResource doesn't exist'''
 
+cache = {}
+
+def cache_response(request, response):
+    cache[request.path] = response
+    return response
+
+def return_cached(request):
+    return cache.get(request.path)
+
+request_middleware = ['return_cached']
+response_middleware = ['cache_response']
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.bind(('0.0.0.0', 8000))
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -31,14 +43,24 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         headers = lines[1:-1]
         print('headers:\n\t' + '\n\t'.join(headers))
 
+        response = None
         request = Request(method, path, headers)
-        if path == '/':
-            response = hello_view(request)
-        elif path == '/headers':
-            response = echo_headers(request)
-        else:
-            response = make_404(request)
-        response = response.encode('utf-8')
+        for layer in request_middleware:
+            response = locals()[layer](request)
+            if response: break
+
+        if not response:
+            print('performing render')
+            if path == '/':
+                response = hello_view(request)
+            elif path == '/headers':
+                response = echo_headers(request)
+            else:
+                response = make_404(request)
+            response = response.encode('utf-8')
+
+        for layer in response_middleware:
+            response = locals()[layer](request, response)
 
         conn.sendall(response)
         conn.close()

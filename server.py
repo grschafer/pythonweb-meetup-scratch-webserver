2f8fe89938130a82cc508fea360fc5f026fe2b70
Greg Schafer <grschafer@gmail.com>
2015-09-23 05:01:08 +0000

    Routing and views

--- server.py
+++ server.py
@@ -1,4 +1,17 @@
 import socket
+from collections import namedtuple
+
+Request = namedtuple('Request', ['method', 'path', 'headers'])
+
+def hello_view(request):
+    return '''HTTP/1.1 200 OK\r\n\r\nHello World!'''
+
+def echo_headers(request):
+    lines = ''.join('<p>{}</p>'.format(h) for h in request.headers)
+    return '''HTTP/1.1 200 OK\r\n\r\n<html>{}</html>'''.format(lines)
+
+def make_404(request):
+    return '''HTTP/1.1 404 Not Found\r\n\r\nResource doesn't exist'''
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.bind(('0.0.0.0', 8000))
@@ -18,10 +31,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         headers = lines[1:-1]
         print('headers:\n\t' + '\n\t'.join(headers))
 
-        response = b'''\
-HTTP/1.1 200 OK
+        request = Request(method, path, headers)
+        if path == '/':
+            response = hello_view(request)
+        elif path == '/headers':
+            response = echo_headers(request)
+        else:
+            response = make_404(request)
+        response = response.encode('utf-8')
 
-Hello World!
-'''
         conn.sendall(response)
         conn.close()

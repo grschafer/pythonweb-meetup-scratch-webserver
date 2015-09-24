6bdf9de2fcfeab86032b50d98f0ad22cfd815053
Greg Schafer <grschafer@gmail.com>
2015-09-24 06:33:56 +0000

    Basic coroutine http-responding server

--- server.py
+++ server.py
@@ -1,34 +1,31 @@
-import socket
-import time
-import os
+import asyncio
 
-def handle_request(client_connection):
-    data = conn.recv(1024)
+async def handle_request(reader, writer):
+    print('handling request')
+    data = await reader.read(1024)
     content = data.decode('utf-8')
 
-    time.sleep(15)
-    response = '''HTTP/1.1 200 OK\r\n\r\nHello World!'''
-    response = response.encode('utf-8')
+    addr = writer.get_extra_info('peername')
+    print('received', content, 'from', addr)
 
-    conn.sendall(response)
-    conn.close()
+    print('send hello world')
+    writer.write(b'''HTTP/1.1 200 OK\r\n\r\nHello World!''')
+    await writer.drain()
 
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind(('0.0.0.0', 8000))
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.listen()
-    while True:
-        conn, addr = sock.accept()
-        print('connected by', addr)
-
-        pid = os.fork()
-        if pid == 0:  # child
-            sock.close()  # close child copy
-            handle_request(conn)
-            conn.close()
-            os._exit(0)  # child exits
-        else:  # parent
-            conn.close()  # close parent copy
+    print('close socket')
+    writer.close()
 
 
+loop = asyncio.get_event_loop()
+coro = asyncio.start_server(handle_request, host='0.0.0.0', port=8000, loop=loop)
+server = loop.run_until_complete(coro)
+
+print('Serving on {}'.format(server.sockets[0].getsockname()))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
